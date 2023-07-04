4edd6047a1a9976e83b2c18874d7a5aac12a6172
kunozga265 <kunozgamlowoka@gmail.com>
2023-07-04 13:49:25 +0200

    Fixed site data types

--- database/migrations/2023_06_21_171942_create_sites_table.php
+++ database/migrations/2023_06_21_171942_create_sites_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('sites', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->string("plot_width");
-            $table->string("plot_height");
-            $table->string("plot_price");
+            $table->double("plot_width");
+            $table->double("plot_height");
+            $table->double("plot_price");
             $table->string("location")->nullable();
             $table->string("district");
             $table->string("layout");
